4de6e980d4c24fdc8a8b98fbf04092ccad35e89a
nikitos-dot <aonn45@gmail.com>
2024-12-04 16:02:42 +0200

    Update Ex3.js

--- Ex3.js
+++ Ex3.js
@@ -1,4 +1,5 @@
-
+// Nikita Sigalov, Roy Horvitz
+//a function that given an arrays counts the number of 0's and prints the amount of 0's in an array
 let arr = [1, 5, 0, 3, 5, 6, 0, 0, 0, 0, 0];
 let zeroNum = 0;
 for (i = 0; i < arr.length; i++) {
